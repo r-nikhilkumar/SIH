dfcf9ab0c7befab2d59143537c795ecad39ec3f0
Himanshu Bisht <himanshubisht885@protonmail.com>
2023-09-24 15:56:01 +0530

    Update SpeechPage.js

--- frontend/src/components/SpeechPage.js
+++ frontend/src/components/SpeechPage.js
@@ -1,12 +1,11 @@
-import React, { useState , useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSpeechSynthesis } from 'react-speech-kit';
-import axios from 'axios';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import doc from '../images/doc1.png';
 
 
 const SpeechPage = () => {
-  const text = "tell symptoms";
+  const [text, setText] = useState(["tell me your symptoms"]);
   const { speak, voices } = useSpeechSynthesis();
   const {
     transcript,
@@ -21,25 +20,33 @@ const SpeechPage = () => {
  
 
   const handleSpeak = () => {
-    speak({text});
+    speak({ text });
   };
+  const Submit = () => {
+    setText(transcript)
+    console.log({transcript})
+  }
 
 
   
 
   return (
     <div style={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-between'}}>
-
       <img src={doc} alt='doctor' style={{width:'100px',height:'auto'}}></img>
-      <div style={{display:'flex',flexDirection:'row',borderStyle:'solid',borderColor:'black',borderWidth:'1px',justifyContent:'space-      between',width:'20vw',height:'auto',alignItems:'center'}}>
-        <div style={{margin:'10px',display:'flex',flexDirection:'column'}}>
-        <button onClick={handleSpeak}>Speak</button>
+      <div style={{display:'flex',flexDirection:'row',borderStyle:'solid',borderColor:'black',borderWidth:'1px',justifyContent:'space-around',width:'50vw',height:'auto',alignItems:'center'}}>
+        <div style={{margin:'10px',display:'flex',flexDirection:'column',alignItems:'center',width:'300px'}}>
+          <div style={{display:'block'}}>
+          <h3 style={{textAlign:'center',color:'blue',fontSize:'50px',display:"block"}}>Tell me Your </h3>
+          <p style={{textAlign:'center',color:'blue',fontSize:'50px'}}>Symptoms</p>
+          </div>
+        <button style={{borderRadius:'50%',aspectRatio:"1/1",width:"100px",backgroundColor:'red'}} onClick={handleSpeak}>Start</button>
       </div>
       <div style={{margin:'10px',display:'flex',flexDirection:'column'}}>
         <p>Microphone: {listening ? 'on' : 'off'}</p>
-        <button onClick={SpeechRecognition.startListening}>Start</button>
-        <button onClick={SpeechRecognition.stopListening}>Stop</button>
+        <button onClick={SpeechRecognition.startListening}>Speak</button>
+        <button onClick={SpeechRecognition.stopListening}>Stop Speaking</button>
         <button onClick={resetTranscript}>Reset</button>
+        <button onClick={Submit}>Submit</button>
         <p>{transcript}</p>
       </div>
       </div>

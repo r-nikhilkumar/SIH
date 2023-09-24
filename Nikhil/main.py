421f67bf634c86229c0273582abf37440372b9e7
Nikhil Kumar <thenikhilkumar1@gmail.com>
2023-09-24 16:14:38 +0530

    1

--- Nikhil/main.py
+++ Nikhil/main.py
@@ -9,24 +9,10 @@ from sklearn.metrics import mean_squared_error, r2_score, confusion_matrix, accu
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})  # Replace with your React app's URL
-@app.route('/api/data',methods=['GET'])
-def get_data(data1):
-# Returning an api for showing in  reactjs
-    data2={
-        'message':data1
-    }
-    return data2
-
 
    
 data = pd.read_excel("Nikhil\Specialist.xlsx")
-# data = data
 
-# for i in col:
-# col
-    
 X = data.drop("specialist", axis=1)
 col = list(X.columns)
 Y = data["specialist"]
@@ -36,7 +22,6 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size=0.8)
 model = RandomForestClassifier(criterion='entropy')
 
 model.fit(x_train, y_train)
-# print(len(col))
 arr = []
 
 print("Tell me all the symptoms you have...")
@@ -54,7 +39,4 @@ x_test_cust = [x_test_cust]
 y_test_cust = [""]
 y_predict = model.predict(x_test_cust)
 print(y_predict)
-get_data(y_predict[0])
-if __name__=="__main__":
-    app.run(debug=True)
 # print(accuracy_score(y_test, y_predict))
